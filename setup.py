2df8e06046f556eb0fcf86368dac8999ac096d1a
ATECCA <kulangjamesgatgong.124189@marwadiuniversity.ac.in>
2025-09-06 18:48:09 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Sep  6 15:23:02 2025
-
 @author: Kulang James Gatgong
 """
 
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='signal_ICT_KulangJamesGatgong_92400133006',
@@ -13,13 +16,13 @@ setup(
     author='Kulang James Gatgong',
     author_email='kulangjamesgatgong.124189@marwadiuniversity.ac.in',
     description='A package for signal processing',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/yourrepository',
+    url='https://github.com/ATECCA/SignalOperations',
     packages=find_packages(),
     install_requires=[
-        'numpy',  # For numerical operations
-        'matplotlib',  # For plotting
+        'numpy',
+        'matplotlib',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
